4a211e37a7c44536d861844699c0b347cac53a21
NikitaE <nikita.e@fora-soft.com>
2022-01-30 18:12:20 +0300

    WIP

--- cmd/dockerService/dockerService.go
+++ cmd/dockerService/dockerService.go
@@ -2,12 +2,14 @@ package dockerService
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
+
+var dockerClient *client.Client
 
 type ContainerDTO struct {
 	State string
@@ -15,17 +17,31 @@ type ContainerDTO struct {
 	Id    string
 }
 
+func GetClient() *client.Client {
+	if dockerClient == nil {
+		cli, err := client.NewClientWithOpts()
+		if err != nil {
+			panic(err)
+		}
+		dockerClient = cli
+	}
+	return dockerClient
+}
+
 func List() []ContainerDTO {
 	var containerList []ContainerDTO
-	cli, err := client.NewClientWithOpts()
-	if err != nil {
-		log.Println(err)
-	} else {
-		containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-		for _, container := range containers {
-			containerDto := ContainerDTO{container.State, strings.Join(container.Names, ", "), container.ID}
-			containerList = append(containerList, containerDto)
-		}
+	cli := GetClient()
+	containers, _ := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	for _, container := range containers {
+		containerDto := ContainerDTO{container.State, strings.Join(container.Names, ", "), container.ID}
+		containerList = append(containerList, containerDto)
 	}
 	return containerList
+}
+
+func WatchEvents() {
+	cli := GetClient()
+	messageChan, _ := cli.Events(context.Background(), types.EventsOptions{})
+	m := <-messageChan
+	fmt.Println(m)
 }
